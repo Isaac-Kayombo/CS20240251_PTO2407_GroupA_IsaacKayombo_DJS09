af07dee797bb22af3a468c6e3f8de23c49122b6e
IsaacK47 <isaackayombo41@gmail.com>
2025-05-09 15:00:10 +0200

    Setting literal types

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import { showReviewTotal, populateUser } from "./utils";
 import { Permissions, LoyaltyUser } from "./enums";
+import { Price, Country } from './types'
 const propertyContainer = document.querySelector('.properties')
 const footer = document.querySelector('.footer')
 
@@ -47,7 +48,7 @@ const properties : {
       firstLine: string;
       city: string;
       code: number;
-      country: string;
+      country: Country;
   };
   contact: [number, string];
   isAvailable: boolean;
